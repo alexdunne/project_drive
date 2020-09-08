418ced2cd53ea40ad3681a23f66a0e3ba2dfc701
Alex Dunne <hi@alexdunne.net>
2020-09-08 14:54:11 +0100

    Added absinthe as a dependency

--- config/dev.exs
+++ config/dev.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # Configure your database
 config :project_drive, ProjectDrive.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "project_drive_dev",
+  username: "project_drive",
+  password: "project_drive",
+  database: "project_drive",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
