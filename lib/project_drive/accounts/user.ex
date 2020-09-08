9fd4943b51f02dd65452d0911e98e680193d141a
Alex Dunne <hi@alexdunne.net>
2020-09-08 15:56:42 +0100

    Created an accounts context with a user module

---
+++ lib/project_drive/accounts/user.ex
@@ -0,0 +1,20 @@
+defmodule ProjectDrive.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :name])
+    |> validate_required([:email, :name])
+  end
+end
